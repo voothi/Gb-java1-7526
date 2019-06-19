c4fc6a847c8bf5370713f140f66a5c5a5341d79a
Denis Novikov <voothi@gmail.com>
2019-06-19 21:32:26 +0300

    ex5 ok

--- src/ru/voothi/lesson1/phw/Phw.java
+++ src/ru/voothi/lesson1/phw/Phw.java
@@ -6,6 +6,9 @@ public class Phw {
         System.out.println(ex2Method(10, 2));
         ex3Method(0);
         ex4Method("Ivan");
+
+        ex5MethodIsLeapYear(1900);
+        ex5MethodIsLeapYear(2000);
     }
 
     static float ex1Method(int a, int b, int c, int d) {
@@ -30,5 +33,15 @@ public class Phw {
 
     static void ex4Method(String firstName) {
         System.out.println("Привет, " + firstName + "!");
+    }
+
+    static void ex5MethodIsLeapYear(int year) {
+        if ((year % 4 == 0) && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else if (year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
