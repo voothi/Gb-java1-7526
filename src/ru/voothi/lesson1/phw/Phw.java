b5829c4f343734b5d6c5cc7113a0b9830694f63d
Denis Novikov <voothi@gmail.com>
2019-06-19 21:12:06 +0300

    ex4 ok

--- src/ru/voothi/lesson1/phw/Phw.java
+++ src/ru/voothi/lesson1/phw/Phw.java
@@ -2,9 +2,10 @@ package ru.voothi.lesson1.phw;
 
 public class Phw {
     public static void main(String[] args) {
-        System.out.println(ex1Method(2,2,4,2));
-        System.out.println(ex2Method(10,2));
+        System.out.println(ex1Method(2, 2, 4, 2));
+        System.out.println(ex2Method(10, 2));
         ex3Method(0);
+        ex4Method("Ivan");
     }
 
     static float ex1Method(int a, int b, int c, int d) {
@@ -25,5 +26,9 @@ public class Phw {
         } else {
             System.out.println("Вы передали положительное число");
         }
+    }
+
+    static void ex4Method(String firstName) {
+        System.out.println("Привет, " + firstName + "!");
     }
 }
