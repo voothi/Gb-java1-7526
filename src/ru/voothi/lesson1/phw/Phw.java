0b998352b2d5f98681957e156fbeab2858970918
Denis Novikov <voothi@gmail.com>
2019-06-19 20:46:19 +0300

    ref

--- src/ru/voothi/lesson1/phw/Phw.java
+++ src/ru/voothi/lesson1/phw/Phw.java
@@ -3,9 +3,19 @@ package ru.voothi.lesson1.phw;
 public class Phw {
     public static void main(String[] args) {
         System.out.println(ex1Method(2,2,4,2));
+        System.out.println(ex2Method(10,2));
     }
 
-    public static float ex1Method(int a, int b, int c, int d) {
+    static float ex1Method(int a, int b, int c, int d) {
         return a * (b + (c / d));
+    }
+
+    static boolean ex2Method(int x, int y) {
+        if (x + y >= 10 && x + y <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+//        return x;
     }
 }
