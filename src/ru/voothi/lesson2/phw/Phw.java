f8834a819b29a8fb6fb8edc8f6b1b34fbaada0d3
Denis Novikov <voothi@gmail.com>
2019-06-24 10:01:19 +0300

    rename meth

--- src/ru/voothi/lesson2/phw/Phw.java
+++ src/ru/voothi/lesson2/phw/Phw.java
@@ -2,10 +2,10 @@ package ru.voothi.lesson2.phw;
 
 public class Phw {
     public static void main(String[] args) {
-        ex1();
+        replacingValuesInArray();
     }
 
-    static void ex1() {
+    static void replacingValuesInArray() {
         int[] arr = new int[5];
         arr[0] = 1;
         arr[1] = 1;
@@ -14,14 +14,13 @@ public class Phw {
         arr[4] = 1;
 //        int[] arr = { 1, 1, 0, 0, 1 };
         for (int i = 0; i < arr.length; i++) {
-            System.out.println("До: Ключ " + i + " Значение " + arr[i]);
+            System.out.println("До:\n Ключ " + i + " Значение " + arr[i]);
             if (arr[i] == 1) {
                 arr[i] = 0;
-                System.out.println("1: Ключ " + i + " Значение " + arr[i]);
             } else if (arr[i] == 0) {
                 arr[i] = 1;
-                System.out.println("2: Ключ " + i + " Значение " + arr[i]);
             }
+            System.out.println("После:\n Ключ " + i + " Значение " + arr[i]);
         }
     }
 
