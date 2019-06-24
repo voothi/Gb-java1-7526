9928eba79421b62cea6fefe163d05beb8a52b085
Denis Novikov <voothi@gmail.com>
2019-06-24 10:02:30 +0300

    refact arr init

--- src/ru/voothi/lesson2/phw/Phw.java
+++ src/ru/voothi/lesson2/phw/Phw.java
@@ -6,13 +6,7 @@ public class Phw {
     }
 
     static void replacingValuesInArray() {
-        int[] arr = new int[5];
-        arr[0] = 1;
-        arr[1] = 1;
-        arr[2] = 0;
-        arr[3] = 0;
-        arr[4] = 1;
-//        int[] arr = { 1, 1, 0, 0, 1 };
+        int[] arr = { 1, 1, 0, 0, 1 };
         for (int i = 0; i < arr.length; i++) {
             System.out.println("До:\n Ключ " + i + " Значение " + arr[i]);
             if (arr[i] == 1) {
