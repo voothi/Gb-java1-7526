3811e8efff242070a06601df4b7865ea12ef3b91
Denis Novikov <voothi@gmail.com>
2019-07-01 12:42:13 +0300

    3

--- src/ru/voothi/lesson4/phw/Employee.java
+++ src/ru/voothi/lesson4/phw/Employee.java
@@ -6,4 +6,32 @@ public class Employee {
     private int telephone;
     private int salary;
     private int age;
+
+    Employee (String fullName, String position, int telephone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFullName () {
+        return this.fullName;
+    }
+
+    public String getPosition () {
+        return this.position;
+    }
+
+    public int getSalary () {
+        return this.salary;
+    }
+
+    public int getTelephone () {
+        return this.telephone;
+    }
+
+    public int getAge () {
+        return this.age;
+    }
 }
