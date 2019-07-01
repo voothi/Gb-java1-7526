213d96ba638a2d7d3e61d64acd9c10ce73d1e570
Denis Novikov <voothi@gmail.com>
2019-07-01 12:26:43 +0300

    1

--- src/ru/voothi/lesson4/phw/Employee.java
+++ src/ru/voothi/lesson4/phw/Employee.java
@@ -1,5 +1,9 @@
 package ru.voothi.lesson4.phw;
 
 public class Employee {
-
+    private String fullName;
+    private String position;
+    private int telephone;
+    private int salary;
+    private int age;
 }
