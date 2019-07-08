47329b8e20e1ba1f975f823f82f608daf51d5033
Denis Novikov <voothi@gmail.com>
2019-07-08 11:07:18 +0300

    fis read

--- src/ru/voothi/lesson6/phw/Main.java
+++ src/ru/voothi/lesson6/phw/Main.java
@@ -1,7 +1,22 @@
 package ru.voothi.lesson6.phw;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        String
+        InputStream in;
+        OutputStream out;
+
+        try {
+            FileInputStream fsi1 = new FileInputStream("TextFile1.txt");
+            int b = fsi1.read();
+            System.out.println(b);
+            fsi1.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
