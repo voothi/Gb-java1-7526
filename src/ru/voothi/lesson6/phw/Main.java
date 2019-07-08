6f9d4f5d64e5eca8c9c5536ccb6f8090ffa4e851
Denis Novikov <voothi@gmail.com>
2019-07-08 11:44:12 +0300

    refact

--- src/ru/voothi/lesson6/phw/Main.java
+++ src/ru/voothi/lesson6/phw/Main.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        InputStream in;
-        OutputStream out;
-
         try {
             Scanner scanner1 = new Scanner(new FileInputStream("TextFile1.txt"));
             while (scanner1.hasNext()) {
@@ -16,9 +13,6 @@ public class Main {
             scanner1.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
 }
