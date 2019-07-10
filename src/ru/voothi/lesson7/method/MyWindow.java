7749f9eebf9eb925fe88c283a4e86a61ab9bd535
Denis Novikov <voothi@gmail.com>
2019-07-10 23:37:20 +0300

    BoxLayout

--- src/ru/voothi/lesson7/method/MyWindow.java
+++ src/ru/voothi/lesson7/method/MyWindow.java
@@ -4,21 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyWindow extends JFrame {
-    public MyWindow () {
-        setTitle("Test Window");
+    public MyWindow() {
+        setBounds(500, 500, 500, 300);
+        setTitle("BoxLayoutDemo");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(300, 300, 400, 400);
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        add(button, BorderLayout.PAGE_START);
-        button = new JButton("Button 2 (CENTER)");
-        button.setPreferredSize(new Dimension(200, 100));
-        add(button, BorderLayout.CENTER);
-        button = new JButton("Button 3 (LINE_START)");
-        add(button, BorderLayout.LINE_START);
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        add(button, BorderLayout.PAGE_END);
-        button = new JButton("5 (LINE_END)");
-        add(button, BorderLayout.LINE_END);
+        JButton[] jbs = new JButton[10];
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); // одну из строк надо закомментировать
+//        setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS)); // одну из строк надо закомментировать
+        for (int i = 0; i < jbs.length; i++) {
+            jbs[i] = new JButton("#" + i);
+            jbs[i].setAlignmentX(CENTER_ALIGNMENT);
+            add(jbs[i]);
+        }
         setVisible(true);
     }
 }
