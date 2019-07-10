d598f6acb8ff74a84a6ec5399700cd7c3406035d
Denis Novikov <voothi@gmail.com>
2019-07-10 23:30:44 +0300

    BoarderLayout test

--- src/ru/voothi/lesson7/method/MyWindow.java
+++ src/ru/voothi/lesson7/method/MyWindow.java
@@ -1,12 +1,23 @@
 package ru.voothi.lesson7.method;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyWindow extends JFrame {
     public MyWindow () {
         setTitle("Test Window");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300, 300, 400, 400);
+        JButton[] jbs = new JButton[5];
+        for (int i = 0; i < jbs.length; i++) {
+            jbs[i] = new JButton("#" + i);
+        }
+        setLayout(new BorderLayout());
+        add(jbs[0], BorderLayout.EAST);
+        add(jbs[1], BorderLayout.WEST);
+        add(jbs[2], BorderLayout.SOUTH);
+        add(jbs[3], BorderLayout.NORTH);
+        add(jbs[4], BorderLayout.CENTER);
         setVisible(true);
     }
 }
