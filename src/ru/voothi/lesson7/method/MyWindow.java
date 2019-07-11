fd869d11e2e5650c917c19606494bfa03f4a3870
Denis Novikov <voothi@gmail.com>
2019-07-11 08:38:02 +0300

    GridLayout

--- src/ru/voothi/lesson7/method/MyWindow.java
+++ src/ru/voothi/lesson7/method/MyWindow.java
@@ -7,10 +7,10 @@ import java.awt.*;
 public class MyWindow extends JFrame {
     public MyWindow() {
         setBounds(500, 500, 500, 500);
-        setTitle("Title");
+        setTitle("GridLayoutDemo");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JButton[] jbs = new JButton[10];
-        setLayout(new FlowLayout());
+        setLayout(new GridLayout(3, 3));
         for (int i = 0; i < jbs.length; i++) {
             jbs[i] = new JButton("#" + i);
             add(jbs[i]);
