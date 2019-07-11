046d66a383764e79f625b2e44520d991f83e3eb9
Denis Novikov <voothi@gmail.com>
2019-07-11 08:13:41 +0300

    FlowLayout

--- src/ru/voothi/lesson7/method/MyWindow.java
+++ src/ru/voothi/lesson7/method/MyWindow.java
@@ -1,19 +1,18 @@
 package ru.voothi.lesson7.method;
 
+import javax.sql.rowset.JdbcRowSet;
 import javax.swing.*;
 import java.awt.*;
 
 public class MyWindow extends JFrame {
     public MyWindow() {
-        setBounds(500, 500, 500, 300);
-        setTitle("BoxLayoutDemo");
+        setBounds(500, 500, 500, 500);
+        setTitle("Title");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JButton[] jbs = new JButton[10];
-        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); // одну из строк надо закомментировать
-//        setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS)); // одну из строк надо закомментировать
+        setLayout(new FlowLayout());
         for (int i = 0; i < jbs.length; i++) {
             jbs[i] = new JButton("#" + i);
-            jbs[i].setAlignmentX(CENTER_ALIGNMENT);
             add(jbs[i]);
         }
         setVisible(true);
