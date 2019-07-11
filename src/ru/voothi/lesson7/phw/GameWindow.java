fef5fe14282d357ae5711c953dfba32d7acef61c
Denis Novikov <voothi@gmail.com>
2019-07-11 19:16:50 +0300

    window w/close

--- src/ru/voothi/lesson7/phw/GameWindow.java
+++ src/ru/voothi/lesson7/phw/GameWindow.java
@@ -3,7 +3,19 @@ package ru.voothi.lesson7.phw;
 import javax.swing.*;
 
 public class GameWindow extends JFrame {
+
+    private static final int WIN_WIDTH = 507;
+    private static final int WIN_HEIGHT = 555;
+    private static final int WIN_POSX = 650;
+    private static final int WIN_POSY = 250;
+
     public GameWindow () {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIN_WIDTH, WIN_HEIGHT);
+        setLocation(WIN_POSX, WIN_POSY);
+        setTitle("TicTacToe");
+        setResizable(false);
+
         setVisible(true);
     }
 }
